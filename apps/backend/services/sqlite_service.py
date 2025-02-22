acd8f6231f60c0d65d1e0ec179ce13a3f58b8328
Brian Chiu <brianchiu@theurbanwriters.com>
2025-02-22 16:56:34 -0500

    implement init db and connection methods

--- apps/backend/services/sqlite_service.py
+++ apps/backend/services/sqlite_service.py
@@ -1,9 +1,23 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.orm import sessionmaker
 from config import Config
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(Config.SQLITE_CONNECTION_URI)
 Base = declarative_base()
 
-LocalSqlSession = sessionmaker(engine, expire_on_commit=False)
+
+class SqliteService:
+    engine = create_engine(Config.SQLITE_CONNECTION_URI)
+    LocalSqlSession = sessionmaker(engine, expire_on_commit=False)
+
+    @classmethod
+    async def init_db(self):
+        Base.metadata.create_all(self.engine)
+
+
+def get_conn():
+    db = SqliteService.LocalSqlSession()
+    try:
+        yield db
+    finally:
+        db.close()
